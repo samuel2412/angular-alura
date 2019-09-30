cce634d149611dbdc7c136557dd754441a59b316
Samuel Alves <samuel241296@hotmail.com>
2019-09-30 14:31:10 -0300

    Parte 1 do curso de angular concluido

---
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+import { PhotoFormComponent } from './photo-form.component';
+
+@NgModule({
+    declarations: [PhotoFormComponent],
+    imports: [
+        CommonModule,
+        HttpClientModule
+    ]
+})
+export class PhotoFormModule { }
---
+++ src/app/photos/photos.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { PhotoModule } from './photo/photo.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoListModule } from './photo-list/photo-list.module';
+
+@NgModule({
+    declarations: [],
+    imports: [
+        PhotoModule,
+        PhotoFormModule,
+        PhotoListModule,
+        HttpClientModule,
+        CommonModule
+    ]
+
+})
+export class PhotosModule{}
